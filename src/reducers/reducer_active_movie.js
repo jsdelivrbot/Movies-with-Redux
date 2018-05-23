4e60dcf1ecd19a5e907882777e3cd65b357a456d
Maria Czerny <mvictoriaczerny@gmail.com>
2018-05-22 23:45:34 -0400

    Additional comments

--- src/reducers/reducer_active_movie.js
+++ src/reducers/reducer_active_movie.js
@@ -1,9 +1,11 @@
 //arugment state is not application state
 export default function(state = null, action) {
+    //wanted action.
     switch(action.type) {
         case 'MOVIE_SELECTED':
             return action.payload;
     }
 
+    //base case. return current state
     return state;
 }
